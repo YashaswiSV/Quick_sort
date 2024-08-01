8435f249461181e19ef17ccc0c5b397e1f0a3d5a
YashaswiSV <vermayashaswi1@gmail.com>
2024-08-01 20:45:08 +0530

    first commit

---
+++ Quick_sortT-5.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int partition(int a[],int lower,int upper){
+	int pivot=a[lower];
+	int i=lower,j=upper;
+	
+	while(i<j){
+		while(a[i]<=pivot)
+		i++;
+		while (a[j]>pivot)
+		j--;
+		if(i<j){
+			swap(a[i],a[j]);
+		}
+	}
+	swap(a[j],a[lower]);
+	return j;
+}
+
+void quick_sort(int a[],int lower,int upper){
+	if(lower<upper){
+		int split=partition(a,lower,upper);
+		quick_sort(a,lower,split-1);
+		quick_sort(a,split+1,upper);
+	}
+}
+
+int main(){
+	int ar[]={21,5,34,56,78,2,34};
+	 int n = sizeof(ar) / sizeof(ar[0]);
+
+    quick_sort(ar,0,n-1);
+
+    for (int i=0; i<n; i++) {
+        cout << ar[i] << " ";
+    }
+    cout << endl;
+	
+}
